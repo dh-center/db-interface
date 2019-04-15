7ab1ed39ed38756eef542a3107c55ac0f5319ae8
Nikita Melnikov <nikmel2803@gmail.com>
2019-04-15 13:53:28 +0300

    remove unused files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,5 @@
 <template>
   <div id="app">
-    <div class="app__header">
-      <nav class="app__nav">
-        <router-link to="/">Catalog</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/sign-up">Sign-Up</router-link>
-        <router-link to="/sign-in">Sign-In</router-link>
-        <router-link to="/sign-out">Sign-Out</router-link>
-      </nav>
-    </div>
-
     <router-view/>
   </div>
 </template>
@@ -22,29 +12,7 @@
   text-align: center;
   color: #2c3e50;
 }
-.app__nav {
-  padding-top: 15px;
-  height: 50px;
-}
-.app__header {
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  height: 50px;
-  background-color: #adb67c;
-}
-.app__nav a {
-  font-weight: bold;
-  color: #ffffff;
-}
-a {
-  text-decoration: none;
-  margin-right: 10px;
-  margin-left: 10px;
-}
-.app__nav a.router-link-exact-active {
-  color: #312656;
-}
+
 body{
   padding: 0;
   margin: 0;
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import store from './store';
 
 Vue.use(Router);
-
-const ifAuthenticated = (to, from, next) => {
-  if (store.getters.isAuthenticated) {
-    next();
-    return;
-  }
-  next('/sign-in');
-};
 
 export default new Router({
   mode: 'history',
@@ -18,30 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'catalog',
-      component: () => import(/* webpackChunkName: "catalog" */ './views/Catalog.vue'),
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/sign-up',
-      name: 'sign-up',
-      component: () => import('./views/SignUp.vue')
-    },
-    {
-      path: '/sign-in',
-      name: 'sign-in',
-      component: () => import('./views/SignIn.vue')
-    },
-    {
-      path: '/sign-out',
-      name: 'sign-out',
-      component: () => import('./views/SignOut.vue'),
-      beforeEnter: ifAuthenticated
+      name: 'home',
+      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
     }
   ]
 });
