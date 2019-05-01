2520da62dd48288323507431c855ef27805a10ba
daniilpashov10 <daniilpashv@gmail.com>
2019-05-01 18:36:35 +0300

    edit relation.js

--- backend/routes/relations.js
+++ backend/routes/relations.js
@@ -4,19 +4,11 @@ const Relation = require('../models/relation');
 
 router.post('/relations', async (req, res) => {
   try {
-    const relation = await Relation.findOne({ locationId: req.body.locationId, relationId: req.body.relationId, personId: req.body.personId, quote: req.body.quote });
-
-    if (relation) await Relation.updateOne({ __id: relation.__id }, { $inc: { __v: 1 } });
-    else {
-      const newRelation = new Relation({
-        locationId: req.body.locationId,
-        relationId: req.body.relationId,
-        personId: req.body.personId,
-        quote: req.body.quote
-      });
-
-      await newRelation.save();
-    }
+    await Relation.update({ locationId: req.body.locationId,
+      relationId: req.body.relationId,
+      personId: req.body.personId },
+    { $push: { quote: req.body.quote } },
+    { upsert: true });
     res.sendStatus(200);
   } catch (error) {
     res.json({ error });
