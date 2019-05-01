21e82e2b16e2bf995a8b049df00be6de7c53ee7f
daniilpashov10 <daniilpashv@gmail.com>
2019-05-01 13:18:21 +0300

    search such relation and update __v

--- backend/routes/relations.js
+++ backend/routes/relations.js
@@ -4,14 +4,22 @@ const Relation = require('../models/relation');
 
 router.post('/relations', async (req, res) => {
   try {
-    const newRelation = new Relation({
-      locationId: req.body.locationId,
-      relationId: req.body.relationId,
-      personId: req.body.personId,
-      quote: req.body.quote
-    });
+    const relation = await Relation.findOne({ locationId: req.body.locationId, relationId: req.body.relationId, personId: req.body.personId, quote: req.body.quote });
 
-    await newRelation.save();
+    console.log(relation);
+    if (relation) await relation.updateOne({ $inc: { __v: 1 } });
+    else {
+      const newRelation = new Relation({
+        locationId: req.body.locationId,
+        relationId: req.body.relationId,
+        personId: req.body.personId,
+        quote: req.body.quote
+      });
+
+      console.log(newRelation);
+      await newRelation.save();
+    }
+    console.log(relation);
     res.sendStatus(200);
   } catch (error) {
     res.json({ error });
