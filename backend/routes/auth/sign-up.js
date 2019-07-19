971c347f1d047583e386fc3299b8335b33ee0386
ilyamore88 <themorozik@gmail.com>
2019-07-20 00:58:48 +0300

    add sign up route

---
+++ backend/routes/auth/sign-up.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/user');
+
+router.post('/sign-up', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body.username, req.body.password);
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+module.exports = router;
