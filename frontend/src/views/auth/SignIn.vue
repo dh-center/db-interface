034d85b8e35e4e9db17fbdeab775a3d9503fdbc9
ilyamore88 <themorozik@gmail.com>
2019-07-20 15:18:03 +0300

    create sign in form

---
+++ frontend/src/views/auth/SignIn.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="signIpForm">
+    <form @submit.prevent="signIn">
+      <h2>Вход в систему</h2>
+      <label for="username">Имя пользователя:</label>
+      <input required type="text" id="username" v-model="username" placeholder="Username">
+      <label for="password">Пароль:</label>
+      <input required type="password" id="password" v-model="password">
+      <div class="errorMessage">{{errorMessage}}</div>
+      <button type="submit">Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SignIn',
+    data() {
+      return {
+        username: '',
+        password: '',
+        errorMessage: ''
+      };
+    },
+    methods: {
+      signIn() {
+
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
