325c7b402725f76a5938cbd63dc68e4503d4fc91
ilyamore88 <themorozik@gmail.com>
2019-07-24 19:30:44 +0300

    add language detector

--- frontend/src/localization/i18next.js
+++ frontend/src/localization/i18next.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import i18next from 'i18next';
 import VueI18Next from '@panter/vue-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 Vue.use(VueI18Next);
 
 const ruLang = require('./languages/ru');
 const enLang = require('./languages/en');
 
+i18next.use(LanguageDetector);
+
 i18next.init({
-  lng: 'en',
+  lng: 'ru',
   resources: {
     ru: { translation: ruLang },
     en: { translation: enLang }
