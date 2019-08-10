f9b879dd5842be79cc62692faa7a0c27d7a801e0
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-11 00:23:11 +0300

    add internationalization support for db

--- backend/models/person.js
+++ backend/models/person.js
@@ -2,14 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  patronymic: String,
-  pseudonym: String,
+  firstName: {
+    type: String,
+    intl: true
+  },
+  lastName: {
+    type: String,
+    intl: true
+  },
+  patronymic: {
+    type: String,
+    intl: true
+  },
+  pseudonym: {
+    type: String,
+    intl: true
+  },
   birthDate: Date,
   deathDate: Date,
-  profession: String,
-  description: String
+  profession: {
+    type: String,
+    intl: true
+  },
+  description: {
+    type: String,
+    intl: true
+  }
 });
 
 module.exports = mongoose.model('persons', personSchema);
