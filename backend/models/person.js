c09e0dc1a555a13d80be465abdee14f2cf5d7c66
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-10 17:16:45 +0300

    add base api for person

--- backend/models/person.js
+++ backend/models/person.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
-  name: String,
+  firstName: String,
+  lastName: String,
   pseudonym: String,
   birthDate: Date,
   deathDate: Date,
--- backend/routes/persons.js
+++ backend/routes/persons.js
@@ -3,6 +3,25 @@ const router = express.Router();
 const Person = require('../models/person');
 
 router.get('/persons', async (req, res) => {
+  const dbQuery = req.query.name ? { name: { $regex: new RegExp(`${req.query.name}`, 'i') } } : {};
+
+  try {
+    const data = await Person.find(dbQuery);
+
+    res.json({ data });
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+router.post('/persons', async (req, res) => {
+  const newPerson = new Person(req.body);
+
+  await newPerson.save();
+  res.sendStatus(201);
+});
+
+router.get('/persons/:id', async (req, res) => {
   try {
     const data = await Person.find({ name: { $regex: new RegExp(`${req.query.name}`, 'i') } });
 
