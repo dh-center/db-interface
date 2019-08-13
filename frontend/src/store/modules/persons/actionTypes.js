ab6bdbe10f0f9f34dda870ac28699350f35c7b6b
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-14 01:13:19 +0300

    remove persons vuex module

--- frontend/src/store/modules/persons/actionTypes.js
+++
@@ -1,9 +0,0 @@
-/**
- * Fetch all persons from API
- */
-export const FETCH_ALL_PERSONS = 'FETCH_ALL_PERSONS';
-
-/**
- * Add new person to the database
- */
-export const CREATE_NEW_PERSON = 'CREATE_NEW_PERSON';
