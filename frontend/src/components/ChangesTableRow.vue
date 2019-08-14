297387a272f8baf21b8d2ddec77625ba1ffef7cf
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-14 03:06:27 +0300

    add component representing entity changes in table

---
+++ frontend/src/components/ChangesTableRow.vue
@@ -0,0 +1,42 @@
+<template>
+  <tr>
+    <td
+      v-for="(field, name) in schema"
+      :key="name"
+    >
+      {{ resultingEntity[name] && resultingEntity[name][dataLanguage] }}
+    </td>
+  </tr>
+</template>
+
+<script>
+  import jsonpatch from 'fast-json-patch';
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'ChangesTableRow',
+    props: {
+      schema: {
+        type: Object,
+        required: true
+      },
+      changes: {
+        type: Array,
+        required: true
+      },
+      entity: {
+        type: Object,
+        required: true
+      }
+    },
+    computed: {
+      ...mapState({
+        dataLanguage: state => state.app.dataLanguage
+      }),
+      resultingEntity() {
+        console.log('lol');
+        return jsonpatch.applyPatch(this.entity, this.changes).newDocument;
+      }
+    }
+  };
+</script>
--- frontend/src/views/persons/ChangesTable.vue
+++ frontend/src/views/persons/ChangesTable.vue
@@ -1,13 +1,47 @@
 <template>
-  <table>
+  <table class="persons-overview__table">
     <tr>
-      <td>lol</td>
+      <td>Actions</td>
+      <td
+        v-for="(field, name) in schema"
+        :key="name"
+      >
+        {{ $t('persons.' + name) }}
+      </td>
     </tr>
+    <ChangesTableRow
+      v-for="changeRecord in changesList"
+      :key="changeRecord._id"
+      :schema="schema"
+      :entity="changeRecord.entity || {}"
+      :changes="changeRecord.changes"
+    />
   </table>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
+  import schema from './schema';
+  import axios from 'axios';
+
+  import ChangesTableRow from '../../components/ChangesTableRow';
   export default {
-    name: 'ChangesTable'
+    name: 'ChangesTable',
+    components: {
+      ChangesTableRow
+    },
+    data() {
+      return {
+        schema,
+        changesList: {}
+      };
+    },
+    computed: mapState({
+      dataLanguage: state => state.app.dataLanguage
+    }),
+    async created() {
+      this.changesList = await axios.get('/persons/changes');
+      console.log(this.changesList);
+    }
   };
 </script>
