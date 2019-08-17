a90ea2d1e144b4d99a9913e56e67d08e64277c60
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-17 15:32:03 +0300

    replace base app styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -49,20 +49,9 @@
   };
 </script>
 
+<style src="./styles/base.css"></style>
+
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  body {
-    padding: 0;
-    margin: 0;
-  }
-
   .app {
     &__header {
       display: flex;
