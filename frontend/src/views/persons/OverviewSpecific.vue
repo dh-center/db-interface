ab2c8d79783cd008779648c55d7e529c8f155123
Nikita Melnikov <nikmel2803@gmail.com>
2019-08-19 19:33:39 +0300

    add diffs to the specific overview

--- frontend/src/views/persons/OverviewSpecific.vue
+++ frontend/src/views/persons/OverviewSpecific.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="persons-create">
+  <div class="persons-overview-specific">
+    <button @click="onEditButtonClick">
+      Edit
+    </button>
     <button @click="savePerson">
       Save
     </button>
-    <PersonInfo
-      v-if="person"
-      ref="personInfo"
-      :person="person"
-    />
+    <div class="persons-overview-specific__container">
+      <PersonInfo
+        v-if="originalPerson"
+        ref="personInfo"
+        :person="originalPerson"
+      />
+      <PersonInfo
+        v-if="edit || changeRecord.changes.length"
+        :person="changedPerson"
+        :editable="edit"
+      />
+    </div>
   </div>
 </template>
 
@@ -16,6 +26,7 @@
   import axios from 'axios';
   import PersonInfo from './Info';
   import PersonModel from '../../models/person';
+  import jsonpatch from 'fast-json-patch';
 
   export default {
     name: 'PersonsOverviewSpecific',
@@ -24,7 +35,10 @@
     },
     data() {
       return {
-        person: null
+        originalPerson: null,
+        edit: false,
+        changeRecord: { changes: [] },
+        changedPerson: null
       };
     },
     computed: mapState({
@@ -32,7 +46,7 @@
     }),
     watch: {
       dataLanguage(newLang) {
-        this.person.language = newLang;
+        this.originalPerson.language = newLang;
         this.$refs.personInfo.setData();
       }
     },
@@ -46,81 +60,44 @@
         let personData = {};
 
         if (personId) {
-          personData = await axios.get(`/persons/${personId}`);
+          personData = await axios.get(`/persons/${personId}`, {
+            params: {
+              withChanges: true
+            }
+          });
         }
-        this.person = new PersonModel(personData, this.dataLanguage);
+
+        if (personData.changes) {
+          this.changeRecord = personData.changes;
+          this.changedPerson = new PersonModel(jsonpatch.applyPatch(JSON.parse(JSON.stringify(personData)), this.changeRecord.changes).newDocument, this.dataLanguage);
+        }
+
+        this.originalPerson = new PersonModel(personData, this.dataLanguage);
+      },
+
+      onEditButtonClick() {
+        this.changedPerson = new PersonModel(jsonpatch.applyPatch(JSON.parse(JSON.stringify(this.originalPerson.data)), this.changeRecord.changes).newDocument, this.dataLanguage);
+        this.edit = true;
       },
 
       async savePerson() {
+        this.edit = false;
         if (this.$route.params.personId) {
-          await axios.put(`/persons/${this.$route.params.personId}`, this.person.data);
+          // update existing originalPerson
+          await axios.put(`/persons/${this.$route.params.personId}`, this.changedPerson.data);
         } else {
-          await axios.post('/persons', this.person.data);
+          // create new originalPerson
+          await axios.post('/persons', this.changedPerson.data);
         }
-
-        this.$router.push({ name: 'persons-overview' });
       }
     }
   };
 </script>
 
 <style>
-  .persons-create {
-    max-width: 450px;
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-
-    &__name {
-      font-family: 'Oranienbaum', serif;
-      font-size: 24px;
-      line-height: 28px;
-      margin-bottom: 10px;
-      color: #2d2d2d;
-    }
-
-    &__info-container {
+  .persons-overview-specific {
+    &__container {
       display: flex;
-      flex-wrap: wrap;
-      border-bottom: 1px solid rgba(0, 0, 0, .2);
     }
-
-    &__info-header {
-      font-family: 'Roboto', sans-serif;
-      font-size: 8px;
-      line-height: 9px;
-      display: block;
-      margin-bottom: 8px;
-      letter-spacing: .2em;
-      text-transform: uppercase;
-      color: #000;
-    }
-
-    &__lifetime,
-    &__profession {
-      flex-basis: 50%;
-      font-family: 'Roboto', sans-serif;
-      font-size: 14px;
-      font-weight: 300;
-      line-height: 16px;
-      padding-left: 20px;
-      margin-bottom: 0;
-      color: #000;
-    }
-
-    &__lifetime {
-      padding-left: 0;
-      border-right: 1px solid rgba(0, 0, 0, .2);
-    }
-
-    &__description {
-      margin-top: 20px;
-    }
-  }
-
-  [contenteditable=true]:empty::before {
-    content: attr(data-placeholder);
-    opacity: 0.5;
-    display: inline;
   }
 </style>
