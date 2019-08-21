c755c0dfbd23d006ceeb6d7d45ca4839bc41384f
ilyamore88 <themorozik@gmail.com>
2019-08-21 23:15:30 +0300

    download dictionaries on demand

--- frontend/src/localization/i18next.js
+++ frontend/src/localization/i18next.js
@@ -5,18 +5,18 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 Vue.use(VueI18Next);
 
-const ruLang = require('./languages/ru');
-const enLang = require('./languages/en');
-
 i18next.use(LanguageDetector);
 
 i18next.init({
-  resources: {
-    ru: { translation: ruLang },
-    en: { translation: enLang }
-  },
+  resources: ['ru', 'en'],
   detection: { order: [ 'navigator' ] },
   fallbackLng: 'en'
 });
+
+if (i18next.language === 'ru') {
+  i18next.addResourceBundle('ru', 'translation', require('./languages/ru'));
+} else if (i18next.language.includes('en')) {
+  i18next.addResourceBundle('en', 'translation', require('./languages/en'));
+}
 
 export default new VueI18Next(i18next);
