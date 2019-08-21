2a360a1b0acedf8da1804f8f9443467fc5d1dfd9
ilyamore88 <themorozik@gmail.com>
2019-08-22 00:06:41 +0300

    fix semaphore command and check again

--- backend/models/location.js
+++ backend/models/location.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const locationSchema = new Schema({
   name: String,
   address: String,
-  constructionDate: Date,
+               constructionDate: Date,
   demolitionDate: Date,
-  buildingType: String,
-  description: String
+                 buildingType: String,
+  'description': String
 });
 
 module.exports = mongoose.model('locations', locationSchema);
