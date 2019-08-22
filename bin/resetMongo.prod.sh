9abc903d6b34deb91528f5a6e1f88fe316273943
ilyamore88 <themorozik@gmail.com>
2019-08-23 00:24:26 +0300

    use docker-compose instead of docker

---
+++ bin/resetMongo.prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'Start script'
+echo 'Connecting to mongoDB container'
+cd ..
+docker-compose -f docker-compose.prod.yml exec mongodb mongo db-interface --eval "db.locations.remove({}) && db.persons.remove({}) && db.addresses.remove({}) && db.changes.remove({}) && db.relations.remove({}) && quit()"
+echo 'Database is clear'
+docker-compose -f docker-compose.prod.yml exec api-prod sh -c "node bin/parserFromGSheets/index.js"
+echo 'New data was save'
+exit 0
--- bin/resetMongo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Start script'
-echo 'Connecting to mongoDB container'
-docker exec -it $(docker ps | grep db-interface_mongodb | awk '{print $1}') mongo db-interface --eval "db.locations.remove({}) && db.persons.remove({}) && db.addresses.remove({}) && db.changes.remove({}) && db.relations.remove({}) && quit()"
-echo 'Database is clear'
-node ../backend/bin/parserFromGSheets/index.js
-echo 'New data was save'
-exit 0
