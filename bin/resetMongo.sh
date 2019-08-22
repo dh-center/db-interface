d69583d15d41c3d4d9810a2ad8c7aec9284deccc
ilyamore88 <themorozik@gmail.com>
2019-08-22 15:49:28 +0300

    remove change and relation collections

--- bin/resetMongo.sh
+++ bin/resetMongo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Start script'
 echo 'Connecting to mongoDB container'
-docker exec -it $(docker ps | grep db-interface_mongodb | awk '{print $1}') mongo db-interface --eval "db.locations.remove({}) && db.persons.remove({}) && db.addresses.remove({}) && quit()"
+docker exec -it $(docker ps | grep db-interface_mongodb | awk '{print $1}') mongo db-interface --eval "db.locations.remove({}) && db.persons.remove({}) && db.addresses.remove({}) && db.changes.remove({}) && db.relations.remove({}) && quit()"
 echo 'Database is clear'
 node ../backend/bin/parserFromGSheets/index.js
 echo 'New data was save'
