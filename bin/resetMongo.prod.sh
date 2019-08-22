db3196f16f45f4fbb46a7c3ccaadc9f0e8c9cc84
ilyamore88 <themorozik@gmail.com>
2019-08-23 01:10:35 +0300

    copy credentials.json to container

--- bin/resetMongo.prod.sh
+++ bin/resetMongo.prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo 'Start script'
+echo 'Copy credentials.json to container'
+docker cp ./credentials.json "$(docker-compose ps -q api)":/usr/src/app/bin/parserFromGSheets
 echo 'Connecting to mongoDB container'
 docker-compose -f docker-compose.prod.yml exec mongodb mongo db-interface --eval "db.locations.remove({}) && db.persons.remove({}) && db.addresses.remove({}) && db.changes.remove({}) && db.relations.remove({}) && quit()"
 echo 'Database is clear'
