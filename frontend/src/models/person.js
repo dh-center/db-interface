1a01e46f1963322a7bdc0a7f0474ede40c193d0f
Nikita Melnikov <nikmel2803@gmail.com>
2019-09-06 02:10:17 +0300

    base person model

--- frontend/src/models/person.js
+++ frontend/src/models/person.js
@@ -1,59 +1,38 @@
-import Model from '../lib/model';
-import schemaTypes from '../lib/schema/types';
-import Schema from '../lib/schema';
+import store from '../store';
 
-/**
- * Class representing a person's data structure
- */
-export default class Person extends Model {
-  /**
-   * Create Person model instance
-   * @param {Object} personData - initial person data
-   * @param {String} language - initial language for person's data
-   */
-  constructor(personData, language) {
-    super(Person.schema);
-    this.data = Person.schema.assign(personData);
-    this.language = language;
+function getMultilingualString() {
+  return {
+    en: '',
+    ru: ''
+  };
+}
+
+export default class Person {
+  constructor(personData) {
+    this.data = personData;
+    this.data.firstName = this.data.firstName || getMultilingualString();
+    this.data.lastName = this.data.lastName || getMultilingualString();
+    this.data.patronymic = this.data.patronymic || getMultilingualString();
+    this.data.description = this.data.description || getMultilingualString();
   }
 
-  /**
-   * Persons schema
-   * @return {Schema}
-   */
-  static get schema() {
-    return new Schema({
-      firstName: schemaTypes.MultilingualString,
-      lastName: schemaTypes.MultilingualString,
-      patronymic: schemaTypes.MultilingualString,
-      description: schemaTypes.MultilingualString,
-      profession: schemaTypes.MultilingualString,
-      birthDate: schemaTypes.String,
-      deathDate: schemaTypes.String
-    });
+  get language() {
+    return store.state.app.dataLanguage;
   }
 
-  /**
-   * Returns full user name
-   * @return {string}
-   */
   get name() {
-    const firstName = this.firstName;
-    const lastName = this.lastName;
-    const patronymic = this.patronymic;
+    const firstName = this.data.firstName[this.language];
+    const lastName = this.data.lastName[this.language];
+    const patronymic = this.data.patronymic[this.language];
 
-    return `${firstName} ${lastName} ${patronymic}`.trim();
+    return `${firstName} ${lastName} ${patronymic}`;
   }
 
-  /**
-   * Sets full user name
-   * @param {String} value - new name
-   */
   set name(value) {
     const [firstName, lastName, ...patronymic] = value.split(' ');
 
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.patronymic = patronymic.join(' ');
+    this.data.firstName[this.language] = firstName;
+    this.data.lastName[this.language] = lastName;
+    this.data.patronymic[this.language] = patronymic.join(' ');
   }
 }
