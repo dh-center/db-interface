83507d86f14304d0ed7b20602c2ab159ae071f6b
Nikit Melnikov <nikmel2803@gmail.com>
2019-09-16 12:50:28 +0300

    display building type

--- backend/models/location.js
+++ backend/models/location.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const getChangesListPlugin = require('./getChangesListPlugin');
+const LocationType = require('./locationType');
 
 const locationSchema = new Schema({
   name: {
@@ -33,5 +34,26 @@ const locationSchema = new Schema({
 });
 
 locationSchema.plugin(getChangesListPlugin);
+
+/**
+ * Get all relations with all their data
+ * @param {Object} query - mongodb query
+ * @returns {Object}
+ */
+locationSchema.statics.fetchAll = function (query) {
+  return this.find(query)
+    .populate('locationTypeId');
+};
+
+/**
+ * Process changed entity before give a response to the client
+ * @param {Object} changedEntity
+ * @returns {Promise<void>}
+ */
+locationSchema.statics.processChangedEntity = async function (changedEntity) {
+  if (changedEntity.locationTypeId) {
+    changedEntity.locationTypeId = await LocationType.findById(changedEntity.locationTypeId);
+  }
+};
 
 module.exports = mongoose.model('locations', locationSchema);
