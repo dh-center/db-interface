6755b88bcab4b396e246ddefe01a1d48f4c734a1
Nikita Melnikov <nikmel2803@gmail.com>
2019-09-18 20:36:43 +0300

    update scripts

--- bin/mongodump.sh
+++ bin/mongodump.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "You must enter exactly 1 command line arguments: env name (prod or dev)"
+    exit 1
+fi
+
 now=$(date '+%d-%m-%Y_%H-%M-%S')
 
-docker-compose -f ./docker-compose.dev.yml exec -T mongodb mongodump --archive > dump_$now.gz
+docker-compose -f ./docker-compose.$1.yml exec -T mongodb mongodump --archive > dump_$now.dump
--- bin/mongorestore.sh
+++ bin/mongorestore.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "dump filename is requred"
+if [ $# -ne 2 ]; then
+    echo "You must enter exactly 2 command line arguments: env name (prod or dev) and dump filename"
     exit 1
 fi
 
-cat $1 | docker-compose -f ./docker-compose.dev.yml exec -T mongodb mongorestore --archive --drop
+cat "$2" | docker-compose -f "./docker-compose.$1.yml" exec -T mongodb mongorestore --archive --drop
