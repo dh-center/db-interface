ba36c6f55ee1f621a64290da4b7485813237d20e
ilyamore88 <themorozik@gmail.com>
2019-09-18 15:16:18 +0300

    update script for prod

--- bin/resetMongo.prod.sh
+++ bin/resetMongo.prod.sh
@@ -6,5 +6,6 @@ echo 'Connecting to mongoDB container'
 docker-compose -f docker-compose.prod.yml exec mongodb mongo db-interface --eval "db.locations.remove({}) && db.persons.remove({}) && db.addresses.remove({}) && db.changes.remove({}) && db.relations.remove({}) && db.relationtypes.remove({}) && db.locationtypes.remove({}) && quit()"
 echo 'Database is clear'
 docker-compose -f docker-compose.prod.yml exec api sh -c "node bin/parserFromGSheets/index.js"
+docker-compose -f docker-compose.dev.yml exec api-dev sh -c "node bin/importPersonsFromJSON.js"
 echo 'New data was save'
 exit 0
