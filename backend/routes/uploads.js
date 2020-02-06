da669b9815ab3ee8d2588e85b08d2e0c6685dd65
Nikita Melnikov <nikmel2803@gmail.com>
2020-02-06 11:06:21 +0300

    improve upload script

--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -3,41 +3,66 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 const path = require('path');
+const uuid = require('uuid/v4');
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_KEY
 });
 
-const fileFilter = (req, file, cb) => {
+/**
+ * Allow onlu JPEG and PNG images
+ * @param {Request} req - Express request for processing
+ * @param {Object} file - file info to upload
+ * @param {function} cb - callback function
+ */
+function fileFilter(req, file, cb) {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
     cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
   }
-};
+}
 
-const upload = multer({
-  fileFilter,
-  storage: multerS3({
-    s3,
+/**
+ * Get Multer middleware for image uploading in certain directory
+ * @param {string} folder - folder in S3 bucket to store in
+ * @return {Multer}
+ */
+function getUploadMiddleware(folder) {
+  return multer({
+    fileFilter,
+    storage: multerS3({
+      s3,
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      bucket: 'st-retrospect-images',
+      metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+      },
+      key: function (req, file, cb) {
+        const key = folder + '/' + uuid() + path.extname(file.originalname);
+
+        cb(null, key);
+      }
+    })
+  });
+}
+
+/**
+ * Upload images for persons
+ */
+router.post('/persons/images', getUploadMiddleware('persons').single('image'), (req, res) => {
+  const requestForImage = {
     bucket: 'st-retrospect-images',
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      console.log(file);
-      console.log(file.originalname);
-      const key = Date.now().toString() + path.extname(file.originalname);
+    key: req.file.key
+  };
 
-      console.log(key);
-      cb(null, key);
+  res.json({
+    payload: {
+      fileKey: req.file.key,
+      url: process.env.IMAGE_HOSTING_ENDPOINT + Buffer.from(JSON.stringify(requestForImage)).toString('base64')
     }
-  })
-});
-
-router.post('/upload', upload.single('image'), (req, res) => {
-  res.sendStatus(200);
+  });
 });
 
 module.exports = router;
